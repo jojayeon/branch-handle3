92722aa55bc0dcdfd59fd99ae81460282e04e9cd
jojayeon <jojayeon6152@gmail.com>
2024-05-30 11:38:28 +0900

    post방식연습

--- app.js
+++ app.js
@@ -13,10 +13,35 @@ const server = http.createServer((req, res)=> {
         res.end(data);
       });
     }
+  }else if (req.method === "POST") {
+    if(req.url === "/submit"){
+      let body = "";
+      req.on("data", (chunk) =>{
+        body += chunk.toString();
+      });
+      req.on("end", ()=>{
+        const parsedData = new URLSearchParams(body);
+        const title = parsedData.get("title");
+
+        const jsonData = {
+          title : title
+        };
+
+        const jsonDataString = JSON.stringify(jsonData, null, a);
+        fs.writeFile(path.join(__dirname, "datajson"), jsonDataString, (err)=>{
+          if(err){
+            console.log(err);
+          }
+          res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+          res.end();
+        });
+      });
+    }
   }
 
 
 });
+
 const p =3000;
 server.listen(p,()=>{
   console.log(`http://localhost:${p}`);
