2d5747293dcb60d756af75e638849cb69d837fde
jojayeon <jojayeon6152@gmail.com>
2024-05-30 10:45:30 +0900

    err표시한 서버

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ const path = require("path");
 const server = http.createServer((req, res)=> {
   if(req.method === "GET") {
     if(req.url === "/"){
-      
+      fs.readFile(path.join(__dirname, "index.html"), (err,data)=>{
+        if(err){
+          console.log(err);
+        }
+        res.writeHead(200, {"content-Type": "text/html; charset = utf-8"});
+        res.end(data);
+      });
     }
   }
 
 
+});
+const p =3000;
+server.listen(p,()=>{
+  console.log(`http://localhost:${p}`);
+});
 
-
-
-
-})
