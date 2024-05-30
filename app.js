fd80dc3d2852b24ee3224faacf3ff6a1761547c2
jojayeon <jojayeon6152@gmail.com>
2024-05-30 10:08:29 +0900

    get방식 작성 중 err부분 작성중

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+
+const server = http.createServer((req, res)=> {
+  if(req.method === "GET") {
+    if(req.url === "/"){
+      
+    }
+  }
+
+
+
+
+
+
+})
