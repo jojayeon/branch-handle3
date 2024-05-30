003f34c014569c60a2e063d072427d9fed908f22
jojayeon <jojayeon6152@gmail.com>
2024-05-30 10:34:09 +0900

    매개변수 err 없이 writeFile

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const path = require("path");
 const server = http.createServer((req, res)=> {
   if(req.method === "GET") {
     if(req.url === "/"){
-      
+      fs.readFile(path.join(__dirname, index.html), (data)=>{
+        res.writeHead(200, {"content-Type": "text/html; charset = utf-8"});
+        res.end(data);
+      })
     }
   }
 
